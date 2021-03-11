24bcd96c300733a7be207fc520ac82c78d804ee7
Excal <20excal07@users.noreply.github.com>
2021-03-11 15:32:44 +0700

    Updated to accomodate weird custom aspect ratios

--- WidescreenCropZoom.ps1
+++ WidescreenCropZoom.ps1
@@ -1,4 +1,18 @@
 Add-Type -AssemblyName System.Windows.Forms
+$AspectRatio_GameX=16    # Specify the game's locked aspect ratio here... (width)
+$AspectRatio_GameY=9     # ... and here (height)
+
+$bounds = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
+
+$AspectRatio_X=$bounds.Width
+$AspectRatio_Y=$bounds.Height
+
+$DisplayRatio=$AspectRatio_X/$AspectRatio_Y
+$GameRatio=$AspectRatio_GameX/$AspectRatio_GameY
+
+$factor=$AspectRatio_GameX/$AspectRatio_X
+
+$zoom = [math]::Round((($AspectRatio_Y*$factor)/$AspectRatio_GameY) * 100)
 
 $code = @"
     [DllImport("user32.dll")]
@@ -15,13 +29,30 @@ if(!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::
     exit
 }
 
+if($zoom -le 100){
+    [System.Windows.Forms.MessageBox]::Show(
+        "It doesn't look like the game's aspect ratio is any wider than your screen's.
+
+Your current display resolution is: $AspectRatio_X x $AspectRatio_Y
+which has a ratio of $DisplayRatio : 1
+
+You defined the game as having a locked $AspectRatio_GameX : $AspectRatio_GameY aspect ratio
+which is equal to $GameRatio : 1
+
+There is no need to zoom and you may launch your game directly instead. This program will now close.",
+        "Sorry!",
+        [System.Windows.Forms.MessageBoxButtons]::OK,
+        [System.Windows.Forms.MessageBoxIcon]::Information
+    )
+    exit
+}
+
 $userInput = Add-Type -MemberDefinition $code -Name UserInput -Namespace UserInput -PassThru
 
 $userInput::BlockInput($true)
 
 [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") | out-null
 [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | out-null
-$bounds = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
 $center = $bounds.Location
 $center.X += $bounds.Width / 2
 $center.Y += $bounds.Height / 2
@@ -42,7 +73,7 @@ Set-ItemProperty -path $registryPath -name "FullScreenTrackingMode" -Value 0    
 Set-ItemProperty -path $registryPath -name "CenterTextInsertionPoint" -Value 0     # "Keep the text cursor:   [Within the edges of the screen]"
 
 Set-ItemProperty -path $registryPath -name "MagnifierUIWindowMinimized" -Value 1   # Start minimized
-Set-ItemProperty -path $registryPath -name "Magnification" -Value 111              # Magnification level @ 111%
+Set-ItemProperty -path $registryPath -name "Magnification" -Value $zoom            # Magnification level @ 111%
 Set-ItemProperty -path $registryPath -name "ZoomIncrement" -Value 1                # Allow 1% zoom increments
 Set-ItemProperty -path $registryPath -name "DisableAudio" -Value 1                 # Disable narrator audio, just in case
 
